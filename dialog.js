d4049752eeed1cf56efdacdd3abbeb5555d6c753
Steven Moore <StevenJamesMoore@gmail.com>
2016-04-26 08:06:33 -0400

    Added code to translate different items from the array to the tile map, found in the top of games.js
    added the sign color switching task (although the graphics are ugly and the task text isn't currently displayed).
    
    I'll work  on this again after a quick nap!
    
    (I'll be back at ~10am if anyone has questions!)

--- dialog.js
+++ dialog.js
@@ -12,6 +12,7 @@ defaultLines["yellow"] = ["Welcome to Yellowville, yo! We speak multicolors as l
 
 var characters = [];
 var story = [];
+var characterName = '';
 
 characters["joe"] = {
 						"name":"Joe",
@@ -28,10 +29,11 @@ characters["rosa"] = {
 							"Do you think you can help us spruce up and add some color to our signs?"
 						],
 						"tasks":[
+							"",
 							"Let's change the APPLES sign to red text.",
 							"Let's change the ORANGES sign to orange text.",
-							"Let's change the LEMON sign to lemon text.",
-							"Let's change the LIME sign to green text.",
+							"Let's change the LEMONS sign to yellow text.",
+							"Let's change the LIMES sign to green text.",
 							"Let's change the BERRIES sign to blue text."
 						],
 						"positiveFeedback":"Great!",
@@ -98,8 +100,9 @@ characters["orangeguard"] = {
 								"hasTask":false
 							};
 
-//takes a character object and an array of lines
+//Can remove this optional text param, was just trying to get it to read the text and positive feedbakc
 function showDialog(character){
+	characterName = character.name;
 	if(character.hasTask){
 		currentLines = character.taskIntro;
 	}
@@ -123,8 +126,10 @@ function showDialog(character){
 		$("#speech-panel-wrapper").find(".body .prev-button").hide();
 		if(currentLines.length < 2)
 			$("#speech-panel-wrapper").find(".body .next-button").hide();
-		else
+		else {
 			$("#speech-panel-wrapper").find(".body .next-button").show();
+			$("#speech-panel-wrapper").find(".body .start-button").hide();
+		}
 		
 		$("#speech-panel-wrapper").find(".panel").animate({"bottom":0},400);
 	})
@@ -142,8 +147,10 @@ function closeDialog(){
 function nextLine(){
 	if(currentIdx < currentLines.length - 1)
 		currentIdx ++;
-	if(currentIdx == (currentLines.length - 1))
+	if(currentIdx == (currentLines.length - 1)) {
 		$("#speech-panel-wrapper").find(".body .next-button").hide();
+		$("#speech-panel-wrapper").find(".body .start-button").show();
+	}
 	if(currentIdx > 0)
 		$("#speech-panel-wrapper").find(".body .prev-button").show();
 	$("#speech-panel-wrapper").find(".body .speech").html(currentLines[currentIdx]);
@@ -154,8 +161,10 @@ function prevLine(){
 		currentIdx --;
 	if(currentIdx == 0)
 		$("#speech-panel-wrapper").find(".body .prev-button").hide();
-	if(currentIdx < currentLines.length - 1)
+	if(currentIdx < currentLines.length - 1) {
 		$("#speech-panel-wrapper").find(".body .next-button").show();
+		$("#speech-panel-wrapper").find(".body .start-button").hide();
+	}
 	$("#speech-panel-wrapper").find(".body .speech").html(currentLines[currentIdx]);
 }
 
--- game.js
+++ game.js
@@ -1,45 +1,84 @@
 var tileSize = 100;
-var map = [	[0,0,0,0,0,0,1,1,1,1,1,1,1],
-			[0,0,0,0,0,0,1,0,0,0,0,1,0],
-			[1,1,1,1,1,1,1,0,0,0,0,1,0],
-			[0,1,0,0,0,0,1,0,0,0,0,1,0],
-			[0,1,0,0,0,0,1,1,1,1,1,1,0],
-			[0,1,0,0,0,0,0,0,0,0,0,0,0],
-			[1,1,0,0,0,0,0,0,0,0,0,0,0],
-			[0,1,0,0,0,0,0,0,0,0,0,0,0],
-			[0,1,0,0,0,0,0,0,0,0,0,0,0]
+var map = [	[0,0,0,0,0,0,0,0,0,0,  1,1,1,0,0,0,0,0,0,0],
+			[0,2,0,3,0,0,3,0,0,0,  0,1,0,0,0,0,0,0,0,0],
+			[0,1,1,1,1,1,1,1,1,0,  1,1,1,1,1,1,1,1,1,1],
+			[0,1,0,"rosa",0,2,0,2,0,0,  0,1,0,0,0,0,0,0,0,0],
+			[0,1,1,"t",1,1,1,1,1,0,  1,1,0,0,0,0,0,0,0,0],
+			[0,1,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0], 
+			
+			[0,1,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0],
+			[0,1,1,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0],
+			[0,0,1,1,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0],
+			[0,0,0,1,1,0,0,0,0,0,  "rosa",0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,1,1,0,0,0,0,  "t",0,0,0,0,0,0,0,0,0],
+			[0,0,0,0,0,1,1,1,1,1,  1,0,0,0,0,0,0,0,0,0],
+	
+			[0,1,0,0,0,0,1,0,0,0,  0,0,1,0,0,0,0,0,0,0], 
+			[0,1,0,0,0,0,0,0,0,0,  0,0,1,0,0,0,0,0,0,0],
+			[0,1,0,0,0,0,0,0,0,0,  0,0,1,1,1,0,0,0,0,0],
+			[0,1,0,0,0,0,0,0,0,0,  0,0,0,0,1,0,0,0,0,0],
+			[0,1,0,0,0,0,0,0,0,0,  0,0,0,0,1,0,0,0,0,0],
+			[0,0,0,0,0,0,0,0,0,0,  0,0,0,0,0,0,0,0,0,0]
 		  ];
+		  
 var floatValue = 10;
 var flyValue = 85;
 var animating = false;
+var offsetY = 0;
+var offsetX = 0;
+var mapWidthTiles = 10 * tileSize;
+var mapHeightTiles = 6 * tileSize;
+var npcs = ["rosa", "amber", "redguard", "orangeguard", "ray", "carrot", "scarlett", "varnon"];
+
+//Needed for Rosa's tasks
+var rosaClasses = ["APPLES", "ORANGES", "LEMONS", "LIMES", "BERRIES"];
+var rosaAnswers = ["red", "orange", "yellow", "green", "blue"];
+var rosaCurrentTask = 0;
 
 function getTileClass(y,x){
 	var tileClass = "tile ";
 	if(map[y][x] == 0){
 		tileClass += "blank";
 	}
+	else if(map[y][x] == 2){ //houses
+		tileClass += "blank house-tile";
+	}
+	else if(map[y][x] == 3){ //buildings
+		tileClass += "blank building-tile";
+	}
+	else if(map[y][x] == "rosa"){ //rosa
+		tileClass += "blank rosa-tile";
+	}
+	else if(map[y][x] == "redguard"){ //redguard-right
+		tileClass += "blank redguard-tile";
+	}
 	else{
-		if(map[y][x+1] != undefined && map[y][x+1] ==1){
+		if(map[y][x+1] != undefined && (map[y][x+1] ==1 || map[y][x+1] == "t")){
 			tileClass += "right-";
 		}
-		if(map[y][x-1] != undefined && map[y][x-1] ==1){
+		if(map[y][x-1] != undefined && (map[y][x-1] ==1 || map[y][x-1] == "t")){
 			tileClass += "left-";
 		}
-		if(map[y-1] != undefined && map[y-1][x] == 1){
+		if(map[y-1] != undefined && (map[y-1][x] == 1 || map[y-1][x] == "t")){
 			tileClass += "top-"
 		}
-		if(map[y+1] != undefined && map[y+1][x] == 1){
+		if(map[y+1] != undefined && (map[y+1][x] == 1 || map[y+1][x] == "t")){
 			tileClass += "bottom-";
 		}
 		tileClass += "open";
 	}
+	
+	if(map[y][x] == "t"){ //talk space
+		tileClass += " talk-tile";
+	}
+	
 	return tileClass;
 }
 
 function isWalkable(y,x){
 	if(y < 0 || y > map.length) return false;
 	else if (x < 0 || x > map[0].length) return false;
-	else return map[y][x] == 1;
+	else return map[y][x] == 1 || map[y][x] == "t";
 }
 
 function floatPlayer(){
@@ -61,17 +100,73 @@ function animateCompanion(){
 	)
 }
 
-function drawMap(){
-	$("#map").height(map.length * 100).width(map[0].length * 100);
-	for (var y = 0; y < map.length; y++){
+//minus y in height and eq();
+function drawMap(yCoord,xCoord){
+	$("#map").height((map.length - yCoord) * 100).width(map[0].length * 100);
+	for (var y = yCoord; y < map.length; y++){
 		$("#map").append("<div class='row'></div>");
-		for (var x = 0; x < map[y].length; x++){
-			$("#map").find(".row").eq(y).append("<div class='"+getTileClass(y,x)+"'></div>");
+		for (var x = xCoord; x < map[y].length; x++){
+			$("#map").find(".row").eq(y - yCoord).append("<div class='"+getTileClass(y,x)+"' data-x='"+x+"' data-y='"+y+"'></div>");
+		}
+	}
+	
+	$('.house-tile').append('<div class="house"></div>')
+	$('.building-tile').append('<div class="building"></div>')
+	$('.rosa-tile').append('<div class="rosa"></div>')
+	$('.redguard-tile').append('<div class="redguard"></div>')
+}
+
+function talk(pY, pX) {
+	var talkable = map[pY][pX];
+	if(talkable == "t") {
+		if(map[pY][pX+1] != undefined && npcs.indexOf(map[pY][pX+1]) > -1 && $("#player").hasClass('right')){
+			showDialog(characters[[pY][pX+1]]);
+		}
+		if(map[pY][pX-1] != undefined && npcs.indexOf(map[pY][pX-1]) > -1 && $("#player").hasClass('left')){
+			showDialog(characters[[pY][pX-1]]);
+		}
+		if(map[pY-1] != undefined && npcs.indexOf(map[pY-1][pX]) > -1 && $("#player").hasClass('top')){
+			showDialog(characters[map[pY-1][pX]]);
+		}
+		if(map[pY+1] != undefined && npcs.indexOf(map[pY+1][pX]) > -1 && $("#player").hasClass('bottom')){
+			showDialog(characters[map[pY+1][pX]]);
 		}
 	}
 }
 
+function rosaTask() {
+	if(rosaCurrentTask >= rosaClasses.length) {
+		$('#map').show();
+		$('#task').hide();
+	}
+	$('#map').hide();
+	$('#task').show();
+	$('#task').append('<img id="rosaImage" src="' + rosaClasses[rosaCurrentTask] + '.png" />');
+	$('.content').html('.' + rosaClasses[rosaCurrentTask] + ' { <br />&nbsp;&nbsp;&nbsp;&nbsp;color:&nbsp;<span id="answer" contenteditable="true"> </span>;<br />}')
+	$('#answer').focus();
+	
+	$('#answer').on('keyup', function() {
+		if(!$(this).html())
+			$(this).html('&nbsp;');
+	});
+}
+
+function startTask() {
+	switch (characterName) {
+		case "Rosa Redrose":
+			//showDialog(characters["rosa"], "tasks");
+			closeDialog();
+			rosaTask();
+			break;
+		case "Scarlett Firework":
+			break;
+		case "Guard of the Red Gate":
+			break;
+	}
+}
+
 $(document).keydown(function(e) {
+	var up, down, left, right = false;
 	var positionX = parseInt($("#player").css("left"));
 	var positionY = parseInt($("#player").css("top"));
 	var shouldAnimate = false;
@@ -85,6 +180,7 @@ $(document).keydown(function(e) {
 			//only move if already facing that direction
 			if($("#player").hasClass("left")){
 				positionX-=tileSize;
+				left = true;
 				shouldAnimate = true;
 			}
 			$("#player").removeClass().addClass("left");
@@ -96,6 +192,7 @@ $(document).keydown(function(e) {
 		else{
 			if($("#player").hasClass("right")){
 				positionX+=tileSize;
+				right = true;
 				shouldAnimate = true;
 			}
 			$("#player").removeClass().addClass("right");
@@ -107,6 +204,7 @@ $(document).keydown(function(e) {
 		else{
 			if($("#player").hasClass("top")){
 				positionY-=tileSize;
+				up = true;
 				shouldAnimate = true;
 			}
 			$("#player").removeClass().addClass("top");
@@ -118,23 +216,77 @@ $(document).keydown(function(e) {
 		else{
 			if($("#player").attr("class") == ""){
 				positionY+=tileSize;
+				down = true;
 				shouldAnimate = true;
 			}
 			$("#player").removeClass();
 		}
 	}
+	var pY = ((positionY+offsetY)/tileSize); //0-5 + offsetY
+	var pX = ((positionX+offsetX)/tileSize); //0-9 + offsetX
 	//prevent keydown when still animating
-	if(!animating && shouldAnimate && isWalkable((positionY/tileSize),(positionX/tileSize))){
+	
+	if(!animating && shouldAnimate && isWalkable(pY,pX)){
 		animating = true;
-		$("#player").animate({"top":positionY+"px","left":positionX+"px"},750,function(){
+		$("#player").animate({"top":positionY+"px","left":positionX+"px"},150,function(){
 			animating = false;
+			
+			if(right && pX % (mapWidthTiles/tileSize) == 0) {
+				offsetX += mapWidthTiles;
+				$('#map').children('.row').remove()
+				drawMap(offsetY/tileSize, offsetX/tileSize);
+				$("#player").css({"top":positionY+"px","left":"0px"});
+				positionX = 0;
+			}
+			if(left && (pX+1) % (mapWidthTiles/tileSize) == 0) {
+				offsetX -= mapWidthTiles;
+				$('#map').children('.row').remove()
+				drawMap(offsetY/tileSize, offsetX/tileSize);
+				var leftOffset = mapWidthTiles - tileSize
+				$("#player").css({"top":positionY+"px","left":leftOffset+"px"});
+				positionX = leftOffset;
+			}
+			
+			if(down && pY % (mapHeightTiles/tileSize) == 0) {
+				offsetY += mapHeightTiles;
+				$('#map').children('.row').remove()
+				drawMap(offsetY/tileSize, offsetX/tileSize);
+				$("#player").css({"top":"0px","left":positionX+"px"});
+				positionY = 0;
+			}
+			if(up && (pY+1) % (mapHeightTiles/tileSize) == 0) {
+				offsetY -= mapHeightTiles;
+				$('#map').children('.row').remove()
+				drawMap(offsetY/tileSize, offsetX/tileSize);
+				var topOffset = mapHeightTiles - tileSize;
+				$("#player").css({"top":topOffset+"px","left":positionX+"px"});
+				positionY = topOffset;
+			}
 		});
 	}
+	
+	talk(pY,pX);
 });
 
 $(document).ready(function(){
+	$('#task').hide();
+	$('#submit').on('click', function() {
+		var answer = $('#answer').html().replace('&nbsp;', '').replace('<br>', '').trim();
+		if(answer == rosaAnswers[rosaCurrentTask]){
+			$('#rosaImage').attr('src', rosaClasses[rosaCurrentTask] + '_DONE.png')
+			
+			//TODO: Dialog
+			rosaCurrentTask++;
+			$('#task').html('');
+			rosaTask();
+		}
+		else {
+			alert('ct: ' + rosaCurrentTask);
+		}
+	});
+	
 	//draw the board
-	drawMap();
+	drawMap(0,0);
 	
 	//float the avatar thingy
 	floatPlayer();
